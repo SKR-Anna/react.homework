b0cbc530318a7576d5a62bdde5fed7015104e609
SKR-Anna <anutich19@gmail.com>
2024-10-10 20:59:53 +0300

    домашка 25-й недели

---
+++ week25/src/App.jsx
@@ -0,0 +1,17 @@
+// import { useState, useEffec } from 'react'
+import Component from './assets/Component'
+import './App.css'
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <Component />
+    </>
+  )
+}
+
+export default App
